5768193a0e8ee3817987c4b8f62d4ae4a1cc857d
atas <andy.torkin@gmail.com>
2021-06-01 10:46:57 +0300

    Formatting

--- interview.py
+++ interview.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from operator import itemgetter
+
 
 # Task 1
 # Create a method that sums every 3rd, 5th, and 9th integer of list
@@ -8,14 +10,13 @@ def sums(numbers, sum_positions=(3, 5, 9)):
         for pos in sum_positions
     )
 
+
 # Task 2
 # Return 3 keys with minimum value in decreasing order
-from operator import itemgetter
-
 def min_keys(input_dict, length=3, reverse=True):
     return tuple(key
                  for key, _ in sorted(input_dict.items(),
-                                      key=itemgetter(1),    
+                                      key=itemgetter(1),
                                       reverse=reverse)[:length])
 
 
@@ -26,5 +27,4 @@ if __name__ == "__main__":
 
     # Test task 2
     input_dict = {i: 20-i for i in range(20)}
-    print(input_dict)
     print(f'task #2: {min_keys(input_dict)}')
