ba3b941ba3485da0d25024cc08a6e9859d15ce1a
Andrey Torkin <andy.torkin@gmail.com>
2021-06-09 14:06:18 +0300

    Fixed stock_picking

--- purchase_autoserial_for_lots/models/stock_picking.py
+++ purchase_autoserial_for_lots/models/stock_picking.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields
 from datetime import datetime as dt
 from datetime import time as t
 
@@ -16,13 +16,11 @@ class PurchaseAutoserialForLots(models.Model):
                     # Change lot_name to new serial
                     # with format [yy][mm][dd]/[3 digit index of todays lines]
                     lines_today_count = StockMove.search_count([
-                        ('date', '>=',
-                         dt.combine(fields.Datetime.now(), t.min)),
-                        ('date', '<=',
-                         dt.combine(fields.Datetime.now(), t.max)),
+                        ('date', '>=', dt.combine(dt.now(), t.min)),
+                        ('date', '<=', dt.combine(dt.now(), t.max)),
                     ])
-                    line.lot_name = fields.Datetime.today().strftime(r'%y%m%d') + \
-                                    '/' + str(lines_today_count).zfill(3)
-                    line.qty_done = line.product_uom_qty
+                    if not line.lot_name:
+                        line.lot_name = fields.Datetime.today().strftime(r'%y%m%d') + \
+                                        '/' + str(lines_today_count).zfill(3)
 
         return super(PurchaseAutoserialForLots, self).button_validate()
